04ce280a8c730ab785a56a392e293b113dc81d11
Ryszard Cichosz-Otawski <56263050+Codestech1@users.noreply.github.com>
2023-06-26 10:38:24 +0200

    Add a simple git class

---
+++ src/main/kotlin/net/onelitefeather/gitpatcher/Git.kt
@@ -0,0 +1,70 @@
+package net.onelitefeather.gitpatcher
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import org.gradle.api.logging.Logging
+import org.gradle.api.tasks.Input
+
+
+abstract class Git(repo: File) {
+
+    companion object {
+        private val LOGGER = Logging.getLogger(Git::class.java)
+    }
+
+    @get:Input
+    var repo: File = repo
+        set(value) {
+            field = value
+            assert(value.exists())
+        }
+
+    @get:Input
+    val committerNameOverride: String? = null
+    @get:Input
+    val committerEmailOverride: String? = null
+
+    private fun decorateEnv(env: MutableMap<String, String>) {
+        if (this.committerNameOverride != null) {
+            env["GIT_COMMITTER_NAME"] = this.committerNameOverride!!
+        }
+
+        if (this.committerEmailOverride != null) {
+            env["GIT_COMMITTER_EMAIL"] = this.committerEmailOverride!!
+        }
+    }
+
+    fun getStatus(): String {
+        return run("status", "z").text()
+    }
+
+    fun getRef(): String {
+        TODO()
+    }
+
+    fun run(name: String, vararg input: String): Command {
+        val args = arrayOf("git", "--no-pager", name.replace('_', '-'), *input)
+        LOGGER.info("gitpatcher: executing {}", args)
+        val builder = ProcessBuilder(*args)
+        this.decorateEnv(builder.environment())
+        builder.directory(repo)
+        return Command(builder.start())
+    }
+
+    class Command(private val process: Process) {
+        fun run(): Int {
+            return process.waitFor()
+        }
+
+        fun execute() {
+            val result = run()
+            assert(result == 0) { "Process returned error code" }
+        }
+
+        fun text(): String {
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            return reader.readText().trim()
+        }
+    }
+}
