f5d978b964a41c1d00e318fd3020ed4faee02b16
Ryszard Cichosz-Otawski <56263050+Codestech1@users.noreply.github.com>
2023-06-26 11:36:33 +0200

    Cleanup Git, add .idea to .gitignore

--- src/main/kotlin/net/onelitefeather/gitpatcher/Git.kt
+++ src/main/kotlin/net/onelitefeather/gitpatcher/Git.kt
@@ -11,6 +11,13 @@ abstract class Git(repo: File) {
 
     companion object {
         private val LOGGER = Logging.getLogger(Git::class.java)
+        private const val GIT_COMMITTER_NAME = "GIT_COMMITTER_NAME"
+        private const val GIT_COMMITTER_EMAIL = "GIT_COMMITTER_EMAIL"
+        private const val STATUS = "STATUS"
+        private const val GIT = "GIT"
+        private const val REV_PARSE = "rev-parse"
+        private const val HEAD = "HEAD"
+        private const val NO_PAGER = "--no-pager"
     }
 
     @get:Input
@@ -25,26 +32,44 @@ abstract class Git(repo: File) {
     @get:Input
     val committerEmailOverride: String? = null
 
+    /**
+     * Adds env variables to the given env map
+     * @param env Map of env variables
+     */
     private fun decorateEnv(env: MutableMap<String, String>) {
         if (this.committerNameOverride != null) {
-            env["GIT_COMMITTER_NAME"] = this.committerNameOverride!!
+            env[GIT_COMMITTER_NAME] = this.committerNameOverride!!
         }
 
         if (this.committerEmailOverride != null) {
-            env["GIT_COMMITTER_EMAIL"] = this.committerEmailOverride!!
+            env[GIT_COMMITTER_EMAIL] = this.committerEmailOverride!!
         }
     }
 
+    /**
+     * Checks status between local and origin branch
+     * @return Git status
+     */
     fun getStatus(): String {
-        return run("status", "z").text()
+        return run(STATUS, "z").text()
     }
 
+    /**
+     * Gets current git commit hash
+     * @return Current git commit hash
+     */
     fun getRef(): String {
-        return run("git", "rev-parse", "HEAD").text().lines().first().trim()
+        return run(REV_PARSE, HEAD).text().lines().first().trim()
     }
 
-    fun run(name: String, vararg input: String): Command {
-        val args = arrayOf("git", "--no-pager", name.replace('_', '-'), *input)
+    /**
+     * Executes a git command
+     * @param name Command name
+     * @param input Command args
+     * @return Command
+     */
+    private fun run(name: String, vararg input: String): Command {
+        val args = arrayOf(GIT, NO_PAGER, name.replace('_', '-'), *input)
         LOGGER.info("gitpatcher: executing {}", args)
         val builder = ProcessBuilder(*args)
         this.decorateEnv(builder.environment())
@@ -53,15 +78,28 @@ abstract class Git(repo: File) {
     }
 
     class Command(private val process: Process) {
+
+        /**
+         * Runs the command
+         * @return Exit value of the process
+         */
         fun run(): Int {
             return process.waitFor()
         }
 
+        /**
+         * Executes the command
+         * @throws AssertionError When exit code is not 0
+         */
         fun execute() {
             val result = run()
             assert(result == 0) { "Process returned error code" }
         }
 
+        /**
+         * Gets the text from the process
+         * @return Text from the process
+         */
         fun text(): String {
             val reader = BufferedReader(InputStreamReader(process.inputStream))
             return reader.readText().trim()
