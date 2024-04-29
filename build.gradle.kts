fc6bb67e47762b36ab59630b2d0b890ec808751c
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-04-29 15:14:28 +0200

    Rename file and fix deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
     id("com.gradle.plugin-publish") version "1.2.1"
+    id("signing")
+    id("net.kyori.indra") version "3.1.3"
+    id("net.kyori.indra.publishing") version "3.1.3"
 }
 
 
@@ -10,17 +13,12 @@ val baseVersion = "0.0.3"
 version = System.getenv("TAG_VERSION") ?: "$baseVersion-dev"
 
 repositories {
-    mavenLocal()
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        mavenContent {
-            includeGroup("codechicken")
-        }
-    }
+    maven("https://eldonexus.de/repository/maven-public/")
 }
 
 dependencies {
-    implementation("codechicken:DiffPatch:1.5.2.1")
+    implementation("dev.onelitefeather:DiffPatch:1.5.3")
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.9.0.202403050737-r")
 
 }
@@ -36,9 +34,37 @@ gradlePlugin {
         create("Glue") {
             id = "dev.onelitefeather.glue"
             displayName = "Plugin for glue patches together into a git repo"
-            description = "A git patcher inspired by soft spoon of paper and glues patches and source together"
+            description = "Glue is a Git patcher that makes file-based changes to repositories. It combines patch files and a Git sub module to a new state. \n" +
+                    "The project is inspired by SpoftSpoon v2 from PaperMC"
             tags = listOf("git", "glue", "paper", "patches")
             implementationClass = "dev.onelitefeather.glue.GluePlugin"
+        }
+    }
+}
+
+indra {
+    publishReleasesTo("eldo", "https://eldonexus.de/repository/maven-releases/")
+    publishSnapshotsTo("eldo", "https://eldonexus.de/repository/maven-snapshots/")
+    javaVersions {
+        target(17)
+        testWith(17)
+    }
+
+    github("OneLiteFeatherNET", "Glue") {
+        ci(true)
+        publishing(false)
+    }
+    mitLicense()
+    signWithKeyFromPrefixedProperties("onelitefeather")
+    configurePublications {
+        pom {
+            developers {
+                developer {
+                    id.set("themeinerlp")
+                    name.set("Phillipp Glanz")
+                    email.set("p.glanz@madfix.me")
+                }
+            }
         }
     }
 }
